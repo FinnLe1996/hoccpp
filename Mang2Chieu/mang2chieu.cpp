77f6dfd5d2ecd396152bdefb7506228bf6ce40b6
FinnLe1996 <lethai@outlook.com>
2021-07-22 20:46:45 +0700

    Udate

--- Mang2Chieu/mang2chieu.cpp
+++ Mang2Chieu/mang2chieu.cpp
@@ -6,78 +6,75 @@ using namespace std;
 int main()
 {
     srand(time(NULL));
-    int row,col;
-    cout<<"Enter Rows Number:";
-    cin>>row;
-    cout<<"Enter Volums Number:";
-    cin>>col;
+    int row, col;
+    cout << "Enter Rows Number:";
+    cin >> row;
+    cout << "Enter Volume Number:";
+    cin >> col;
     int M[row][col];
-    for(int i=0;i<row;i++)
+    for (int i = 0; i < row; i++)
     {
-        for(int j=0;j<col;j++)
+        for (int j = 0; j < col; j++)
         {
-            M[i][j]=rand()%100;
+            M[i][j] = rand() % 100;
         }
     }
-    cout<<"Mang Sau Khi Nhap:\n";
-    for(int i=0;i<row;i++)
+    cout << "Mang Sau Khi Nhap:\n";
+    for (int i = 0; i < row; i++)
     {
-        for(int j=0;j<col;j++)
+        for (int j = 0; j < col; j++)
         {
-           cout<<M[i][j]<<"\t";
+            cout << M[i][j] << "\t";
         }
-        cout<<"\n";
+        cout << "\n";
     }
     /*
     Function Input Rows
 	Begin */
-    cout<<"Ban Muon Xuat Dong Nao::";
+    cout << "Ban Muon Xuat Dong Nao::";
     int r;
-    cin>>r;
-    for(int i=0;i<row;i++)
-    cout<<M[r][i]<<"\t";
-    cout<<"\nBan Muon Xuat Cot Nao";
+    cin >> r;
+    for (int i = 0; i < row; i++)
+        cout << M[r][i] << "\t";
+    cout << "\nBan Muon Xuat Cot Nao";
     int c;
-    cin>>c;
-    for(int i=0;i<col;i++)
-    cout<<M[i][c]<<"\t";
-    if(row==col)
+    cin >> c;
+    for (int i = 0; i < col; i++)
+        cout << M[i][c] << "\t";
+    if (row == col)
     {
-    	cout<<"\n Xuat Ðuong Cheo Chinh"<<endl;
-    	for(int i=0;i<row;i++)
-    	cout<<M[i][i]<<"\t";
-    	cout<<"\n Xuat Ðuong Cheo Phu"<<endl;
-    	for(int i=0;i<col;i++)
-    	cout<<M[i][row-i-1]<<"\t";
-    	
-	}
- /* 
+        cout << "\n Xuat Ðuong Cheo Chinh" << endl;
+        for (int i = 0; i < row; i++)
+            cout << M[i][i] << "\t";
+        cout << "\n Xuat Ðuong Cheo Phu" << endl;
+        for (int i = 0; i < col; i++)
+            cout << M[i][row - i - 1] << "\t";
+    }
+    /* 
        Update Function Search Value
        Author:FinnLe
        */
-       
-       
-   cout<<"\nTim Kiem Gia Tri: ";
+
+    cout << "\nTim Kiem Gia Tri: ";
     int k;
-    cin>>k;
+    cin >> k;
     int kt;
-    for(int i=0;i<row;i++)
+    for (int i = 0; i < row; i++)
     {
-    	for(int j=0;j<col;j++)
-    	{
-    	if(k==M[i][j])
-    	{
-    		kt++;
-    		
-			}
-	    }
-	}
-	if(kt>0)
-	{
-		cout<<"\t GT "<<k<<" Duoc Tim Thay"<< kt <<" lan"<<endl;
-	}
-	else
-	cout<<"\tKhong Tim Thay Gia Tri:"<<k<<endl; 
-	
+        for (int j = 0; j < col; j++)
+        {
+            if (k == M[i][j])
+            {
+                kt++;
+            }
+        }
+    }
+    if (kt > 0)
+    {
+        cout << "\t GT " << k << " Duoc Tim Thay" << kt << " lan" << endl;
+    }
+    else
+        cout << "\tKhong Tim Thay Gia Tri:" << k << endl;
+
     return 0;
 }
